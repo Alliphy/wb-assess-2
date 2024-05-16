5c26b11493e172506f893a514c1b98a777579ac8
Alliphy <ruebaker29@gmail.com>
2024-05-16 13:37:37 -0600

    html and css fleshed out

--- src/objects.js
+++ src/objects.js
@@ -13,7 +13,10 @@
 //     { firstName: 'Karlach', lastName: 'Cliffgate', location: 'Avernus' }
 //   ]);
 //   => ['Gale Dekarios', 'Wyll Ravengard', 'Karlach Cliffgate'];
-function getNames(people) {}
+function getNames(people) {
+  // map through people array and return first and last name
+  return people.map(person => `${person.firstName} ${person.lastName}`);
+}
 
 // Given an object representing a person, return their full name (first name and last name).
 // You MUST use object destructuring in your solution.
@@ -24,7 +27,11 @@ function getNames(people) {}
 // Ex.:
 //   getName({ firstName: 'Gale', lastName: 'Dekarios', location: 'Waterdeep' });
 //   => 'Gale Dekarios'
-function getNameUsingDestructuring(person) {}
+function getNameUsingDestructuring(person) {
+ const { firstName, lastName } = person;
+
+ return `${firstName} ${lastName}`;
+}
 
 // Given an array of objects representing people, return a new array of the
 // people matching the given location.
@@ -43,14 +50,17 @@ function getNameUsingDestructuring(person) {}
 //     { firstName: 'Wyll', lastName: 'Ravengard', location: "Baldur's Gate" },
 //     { firstName: 'Astarion', lastName: 'Ancunin', location: "Baldur's Gate" }
 //   ];
-function getPeopleByLocation(people, location) {}
+function getPeopleByLocation(people, location) {
+  // use filter to filter out the location we want from within the array
+  return people.filter( person => person.location === "Baldur's Gate")
+}
 
 // Translate a phrase to pirate talk.
 //
 // Given an English phrase, use the EN_PIRATE_LOOKUP object to translate words
 // to pirate talk. Words that aren't listed in the lookup object should not be
 // translated and should pass through unchanged.
-//
+
 // The given phrase will be normalized so it'll never contain punctuation and
 // will only consist of lowercased letters.
 //
@@ -72,7 +82,13 @@ const EN_PIRATE_LOOKUP = {
   hello: 'ahoy',
 };
 
-function translateToPirateTalk(phrase) {}
+function translateToPirateTalk(phrase) {
+  // using phrase.split to split the phrase into words
+  // .map through the EN_PIRATE_LOOPUP[word] || word to iterate through the words in order to translate them or keep them unchanged based on what we have on our list.
+  // then .join to add the translated words back into our phrase
+  return phrase.split(" ").map(word => EN_PIRATE_LOOKUP[word] || word).join
+(" ");
+}
 
 // Return the number of occurrences of each word in a string.
 // This function doesn't handle punctuation and is case-sensitive, so you can
@@ -81,7 +97,27 @@ function translateToPirateTalk(phrase) {}
 // Ex.:
 //   wordCount('hello world')
 //   => { hello: 1, world: 1 }
-function wordCount(str) {}
+
+
+function wordCount(str) {
+
+  // reduce takes in our three arguments
+  // the accumulator is empty in this instance as we don't know the current cord count
+  // current index of the word in the array. Do I need this somewhere? 
+
+    return str.split().reduce((count, word) => {
+      // count[word] accesses count for our current word 
+      // || 0 if the word doesn't exist count[word] will be undefined. it will evaluate to a big 0
+      // then it will add keep looking for a new word with our + 1
+      // return our count
+      // this hurt my brain. rip to sanity
+    count[word] = (count[word] || 0 + 1);
+
+    return count;
+  }, {})
+}
+
+// How to get in one line in our console return 
 
 // Given an object representing a bug, return true if the given bug is
 // available in the given month.
@@ -103,7 +139,11 @@ function wordCount(str) {}
 //     }
 //   }, 1);
 //   => true
-function isBugAvailable(bug, month) {}
+function isBugAvailable(bug, month) {
+  // check if month is available by using .includes()in the bug's availability within months
+
+  return bug.availability.months.includes(month);
+}
 
 // Given an array of objects representing bugs, return an object that'll be
 // used to build a calendar. The keys of the object should be the months of the
@@ -146,7 +186,35 @@ function isBugAvailable(bug, month) {}
 //     12: [],
 //   }
 
-function buildBugHuntCalendar(bugs) {}
+function buildBugHuntCalendar(bugs) {
+
+  // create empty object to store our bugs :)
+  const calendar = {}
+
+  // loop through each month (1-12) and keep adding to total of our iterator month++
+  for (let month = 1; month <= 12; month++) {
+    calendar[month] = [];
+  }
+
+  // loop through bugs using forEach
+bugs.forEach(bug => {
+  // iterate through available months using another forEach
+  bug.availability.months.forEach(availableMonth => {
+    // check if month is in valid range of months 1-12
+    if (availableMonth >= 1 && availableMonth <= 12) {
+      // add bugs name to calendar for each month that is valid to bug
+      calendar[availableMonth].push(bug.name);
+    } else {
+      // incase something is wrong we throw a warning
+      console.warn (`Invalid month for ${bug.name} : ${availableMonth}`)
+    }
+  })
+})
+
+// return completed object
+return calendar;
+
+}
 
 export {
   buildBugHuntCalendar,
